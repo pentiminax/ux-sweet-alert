ce5c7fdb6029e8d47c9c7a727d1e4c4d04299890
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-06-03 22:56:36 +0200

    Allow Callback

--- assets/dist/controller.js
+++ assets/dist/controller.js
@@ -8,6 +8,22 @@ class default_1 extends Controller {
     async connect() {
         const toasts = this.viewValue;
 
+        document.addEventListener('ux-sweet-alert:alert:added', async(e) => {
+            const alert = e.detail['alert'];
+
+            const result = await Swal.fire(alert);
+
+            let callback = e.detail['callback'] ?? null;
+
+            if (typeof callback === 'string' && typeof window[callback] === 'function') {
+                callback = window[callback];
+            }
+
+            if (typeof callback === 'function') {
+                callback(result);
+            }
+        });
+
         for (const toast of toasts) {
             const result = await Swal.fire(toast);
 
