2665bbf04c2096c32e122e410c4c23141c87675d
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2026-01-12 11:25:26 +0100

    docs: track Astro content

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://pentiminax.github.io',
+  base: '/ux-sweet-alert',
   integrations: [
     starlight({
       title: 'UX SweetAlert',
